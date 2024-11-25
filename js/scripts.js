7527cf7b37f18600f1b465e217b847887fd0bad8
Josh Butcher <sjwb1999@gmail.com>
2024-11-25 09:38:37 -0800

    update both HTML and JS bodies.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,30 @@
 window.addEventListener("load", function() {
-  const h1 = document.querySelector("h1");
-  const h1Two = document.querySelector("h1");
-  const h2 = document.querySelector("h2");
-  const p = document.querySelector("p"); 
-  const imgElement = document.querySelector("img");
-  const ulElement = document.querySelector("ul");
-  const a = document.querySelector("a");
+  const body = document.querySelector("body");
 
-  const H1Element = document.createElement("h1");
-  const H1Element2 = document.createElement("h1");
-  const li1Element = document.createElement("li");
-  const li2Element = document.createElement("li");
-  const pElement = document.createElement("p");
-  const H2Element = document.createElement("h2");
+  const h1One = document.createElement("h1");
+  const h1Two = document.createElement("h1");
+  const li1 = document.createElement("li");
+  const li2 = document.createElement("li");
+  const pText = document.createElement("p");
+  const h2 = document.createElement("h2");
   const aElement = document.createElement("a");
+  const ul = document.createElement("ul");
+  const img = document.createElement("img");
 
-  H1Element.append("Webpage Recreation Practice");
-  h1.append(H1Element);
+  
+  h1One.append("Webpage Recreation Practice");
+  
 
-  pElement.append("The HTML of this webpage was created with JavaScript.");
-  p.append(pElement);
-  p.after(imgElement);
-  imgElement.src = "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900";
-  H1Element2.append("test");
-  h1Two.append(H1Element2);
-  h1Two.before(imgElement);
-  ulElement.setAttribute("id", "ulList");
-  li1Element.append("It is endemic to the mountains of Colombia.");
-  li2Element.append("It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet.");
-  document.getElementById("ulList").append(li1Element, li2Element);
-  H2Element.append("Source");
-  h2.append(H2Element);
-  aElement.href = "#"
-  aElement.append("Wikipedia");
-  a.append(aElement);
+  pText.append("The HTML of this webpage was created with JavaScript.");
+  h1Two.append("Facts about the Multicolored Tanager");
+  img.src = "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900"  
+  h2.append("Source");
+  aElement.href = "#";
+  aElement.append("test");
+  body.append(h1One, pText,img, h1Two, ul, h2, aElement);
 
+  li1.append("test");
+  document.querySelector("ul").append(li1, li2);
+  
+  
 })
